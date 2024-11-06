1a3abd39f1284edd7d1b644eba47853198383775
47-z <160566250+47-z@users.noreply.github.com>
2024-11-06 13:23:58 -0500

    Add pink boots

--- Codsmetics/main.gd
+++ Codsmetics/main.gd
@@ -7,6 +7,7 @@ func _ready():
 	# Accessories
 	Lure.add_content(ID,"accessory_dark_antlers","mod://Accessory/accessory_dark_antlers.tres")
 	Lure.add_content(ID,"accessory_cowboy_boots","mod://Accessory/accessory_cowboy_boots.tres")
+	Lure.add_content(ID,"accessory_cowboy_boots_pink","mod://Accessory/accessory_cowboy_boots_pink.tres")
 	
 	# Overshirts
 	Lure.add_content(ID,"overshirt_overall_blue","mod://Overshirts/overshirt_overall_blue.tres")
